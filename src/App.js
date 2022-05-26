ef5372349bd1a2b0e13a3e606681da79f2bdef36
Jamal Smilez <jgrigz1@gmail.com>
2022-05-26 18:00:39 -0400

    halfway there

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <h1>Hello From React</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Dr Appointment',
+        day: 'Feb 5th at 2:30 p.m.',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'orgy',
+        day: 'Feb 5th at 3:00 p.m.',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'poop',
+        day: 'Feb 5th at 3:30 p.m.',
+        reminder: true,
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Tasks
